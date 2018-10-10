cb4882d91b6180647507989293dd7f0445e679c6
onejeet <jitendra909@gmail.com>
2018-10-10 22:41:35 +0530

    Accessibility and Focus Implementation

--- js/main.js
+++ js/main.js
@@ -157,7 +157,6 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.alt = restaurant.name + " Restaurant";
@@ -176,12 +175,15 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
+  const button = document.createElement('button');
+  button.setAttribute('tabindex','-1');
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  button.append(more);
+  li.append(button);
 
-  return li
+  return li;
 }
 
 /**
